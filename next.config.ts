845c5be67c9af7a73feddb2570f59658e487b5d6
Haris Herdiansyah <haris54237@gmail.com>
2024-12-28 08:31:26 +0700

    change env config S3

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     DIRECT_URL: process.env.DIRECT_URL,
+    S3_BUCKET: process.env.S3_BUCKET,
+    S3_REGION: process.env.S3_REGION,
+    S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
+    S3_SECRET_ACCESS_KE: process.env.S3_SECRET_ACCESS_KE
   },
   images: {
     remotePatterns: [
